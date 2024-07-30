3646956933ce8bd26f33fdeca290cdc7c490048e
yatatsu <yatatsukitagawa@gmail.com>
2024-07-30 11:00:19 +0900

    Fix Carthage

---
+++ scripts/carthage.sh
@@ -0,0 +1,12 @@
+# carthage.sh
+# Usage example: ./carthage.sh build --platform iOS
+
+set -euo pipefail
+
+xcconfig=$(mktemp /tmp/static.xcconfig.XXXXXX)
+trap 'rm -f "$xcconfig"' INT TERM HUP EXIT
+
+echo "IPHONEOS_DEPLOYMENT_TARGET = 11.0" >> $xcconfig
+
+export XCODE_XCCONFIG_FILE="$xcconfig"
+carthage "$@"
